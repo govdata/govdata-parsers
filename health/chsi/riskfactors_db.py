b2c173976d8bdcaacad45195e327a0505ec5515e
sharon <sharon@sharon-laptop.(none)>
2011-02-23 11:51:18 -0500

    Modified the metadata in the chsi dataset

--- health/chsi/riskfactors_db.py
+++ health/chsi/riskfactors_db.py
@@ -9,9 +9,10 @@ import sets
 BASEURL = 'http://www.data.gov/raw/2159/csv'
 FILENAME = 'RISKFACTORSANDACCESSTOCARE.csv'
 
+def create_metadata():
 # Metadata
-name = 'riskfactors'
-source = {
+    name = 'riskfactors'
+    source = {
 	"agency": {"shortName": "DHHS", "name": "Department of Health and Human Services"},
 	"subagency": {"shortName": "CDC", "name": "Centers for Disease Control and Prevention"},
 	"topic": {"name": "Health and Nutrition"},
@@ -19,17 +20,31 @@ source = {
 	"program": {"shortName": "CHSI", "name": "Community Health Status Indicators"},
 	"dataset": {"shortName": "ohdc", "name": "Community Health Status Indicators (CHSI) to Combat Obesity, Heart Disease and Cancer"}
 }
-metadata = {
-	'title' = 'Community Health Status Indicators (CHSI) to Combat Obesity, Heart Disease and Cancer',
-	'description' = "Community Health Status Indicators (CHSI) to combat obesity, heart disease, and cancer are major components of the Community Health Data Initiative. This dataset provides key health indicators for local communities and encourages dialogue about actions that can be taken to improve community health (e.g., obesity, heart disease, cancer). The CHSI report and dataset was designed not only for public health professionals but also for members of the community who are interested in the health of their community. The CHSI report contains over 200 measures for each of the 3,141 United States counties. Although CHSI presents indicators like deaths due to heart disease and cancer, it is imperative to understand that behavioral factors such as obesity, tobacco use, diet, physical activity, alcohol and drug use, sexual behavior and others substantially contribute to these deaths.",
-	'keywords' = ['Obesity','CHSI','health','data','community','indicators','interventions','performance','measurable','life expectancy','mortality','disease','prevalence','risk','factors','behaviors','socioeconomic','environments','access','cost','quality','warehouse','heart','cancer'],
-	'uniqueIndexes' = ['Location'],
-	'sliceCols' = [['Location']],
-	'columnGroups' = {
-		'spaceColumns' = ['Location'],
-		'labelColumns' = ['Location']
-	}
+    Y = tb.tabarray(SVfile = 'DATAELEMENTDESCRIPTION.csv')
+    metadata = {
+	'title':'Community Health Status Indicators (CHSI) to Combat Obesity, Heart Disease and Cancer',
+	'description':"Community Health Status Indicators (CHSI) to combat obesity, heart disease, and cancer are major components of the Community Health Data Initiative. This dataset provides key health indicators for local communities and encourages dialogue about actions that can be taken to improve community health (e.g., obesity, heart disease, cancer). The CHSI report and dataset was designed not only for public health professionals but also for members of the community who are interested in the health of their community. The CHSI report contains over 200 measures for each of the 3,141 United States counties. Although CHSI presents indicators like deaths due to heart disease and cancer, it is imperative to understand that behavioral factors such as obesity, tobacco use, diet, physical activity, alcohol and drug use, sexual behavior and others substantially contribute to these deaths.",
+	'keywords':['Obesity','CHSI','health','data','community','indicators','interventions','performance','measurable','life expectancy','mortality','disease','prevalence','risk','factors','behaviors','socioeconomic','environments','access','cost','quality','warehouse','heart','cancer'],
+	'uniqueIndexes':['Location'],
+	'sliceCols':[['Location']],
+	'columnGroups':{
+		'spaceColumns':['Location'],
+		'labelColumns':['Location']
+	},
+
+       'columnDescriptions': dict([(y['COLUMN_NAME'],y['DESCRIPTION']) for y in Y]),
+       'source':{
+        "agency": {"shortName": "DHHS", "name": "Department of Health and Human Services"},
+        "subagency": {"shortName": "CDC", "name": "Centers for Disease Control and Prevention"},
+        "topic": {"name": "Health and Nutrition"},
+        #"subtopic": {"name": "Release Quantity Data"},
+        "program": {"shortName": "CHSI", "name": "Community Health Status Indicators"},
+        "dataset": {"shortName": "ohdc", "name": "Community Health Status Indicators (CHSI) to Combat Obesity, Heart Disease and Cancer"}
+         }
+
 }
+    return metadata
+
 # TODO: Insert metadata into MongoDB
 
 def download():
@@ -75,14 +90,13 @@ def csvparser():
 	header = F.readline()
 	headersplit = header.split(',')
 	updateDate = headersplit[-1]
-	X = tb.tabarray(SVfile = fname,skiprows=0,names=headersplit[:-1])
+	X = tb.tabarray(SVfile = FILENAME,skiprows=0,names=headersplit[:-1])
 
         # Combine location information into one 'Location' column
 	locations = [buildLocation(locCodes, locTuple) for locTuple in X[locFields]]
         X = X.addcols(locations, 'Location')
         X = X.deletecols(locFields)
 
-	# TODO: Insert data into MongoDB
-
+        X.saveSV('results.csv', metadata=True)
 	# TODO: Clean up downloaded CSV file
 	#system('rm -f %s' % fname)
